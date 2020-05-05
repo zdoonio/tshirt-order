7ecf58a464036adfca26ace7fed6076f399a1549
dzdunczyk <dominik.zdunczyk@taxustit.com.pl>
2020-05-05 17:48:48 +0200

    refactor: project cleanup

--- app/controllers/DemoController.scala
+++ app/controllers/DemoController.scala
@@ -1,33 +1,23 @@
 package controllers
 
 import javax.inject._
-import mapper.JsonWriter
-import models.{Category, CategoryDTO, Product, ProductDTO}
-import play.api.libs.json.Json
 import play.api.mvc._
-import scalikejdbc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
 class DemoController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def getProducts = Action { implicit request =>
-    val products = Product.findAll()
-
-    Ok(Json.obj("products" -> products.map ( product =>
-      JsonWriter.productWrites.writes(ProductDTO(product.id, product.name, product.brand, product.price,
-        Some(CategoryDTO(Category.findBy(sqls"id = ${product.categoryId}"))),
-        product.createdAt, product.updatedAt, product.depotPrice))
-    )))
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
+  def get = Action { implicit request =>
+    Ok("Hello API")
   }
 
 }
--- app/models/ProductDTO.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import java.time.ZonedDateTime
-
-/**
-  * Created by ${USER_dabrowski} on 04.02.2020.
-  */
-case class ProductDTO(id: Long, name: Option[String] = None, brand: Option[String] = None, price: Option[BigDecimal] = None,
-                        category: Option[CategoryDTO] = None, createdAt: ZonedDateTime, updatedAt: ZonedDateTime, depotPrice: Option[BigDecimal] = None)
